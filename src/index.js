e40b34f12e695788b34829d29d5f53b9e0503ba4
Adam Colvin <adamcolv@icloud.com>
2024-09-21 17:00:49 +0100

    Fetch and parse rovers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello Mars Rovers");
+import dotenv from "dotenv";
+dotenv.config();
+
+const api_key = process.env.NASA_API_KEY;
+const ROVERS_API_URL = `https://api.nasa.gov/mars-photos/api/v1/rovers`;
+
+async function fetchRovers() {
+  const url = ROVERS_API_URL + `?api_key=${api_key}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const data = await response.json();
+    return data["rovers"];
+  } catch (error) {
+    console.log(`Error fetching image from Rover:`, error);
+    return [];
+  }
+}
+
+const rovers = await fetchRovers();
+console.log(rovers);
